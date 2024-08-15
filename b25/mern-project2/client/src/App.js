3e3128bb38cfbc6b6f65f39bf58bf8dbebddd2e7
koritala bhargavi <152010488+KoritalaBhargavi@users.noreply.github.com>
2024-08-15 17:08:23 +0530

    App.js

--- b25/mern-project2/client/src/App.js
+++ b25/mern-project2/client/src/App.js
@@ -239,4 +239,21 @@ const App = () => {
                 </p>
               </div>
               <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
-                <h6 className="text-uppercase
+                                <h6 className="text-uppercase fw-bold mb-4">
+                  <i className="fas fa-address-book me-3"></i>Contact
+                </h6>
+                <p><i className="fas fa-home me-3"></i> New York, NY 10012, US</p>
+                <p><i className="fas fa-envelope me-3"></i> info@example.com</p>
+                <p><i className="fas fa-phone me-3"></i> + 01 234 567 88</p>
+                <p><i className="fas fa-print me-3"></i> + 01 234 567 89</p>
+              </div>
+            </div>
+          </div>
+        </section>
+      </footer>
+    </div>
+  );
+};
+
+export default App;
+
