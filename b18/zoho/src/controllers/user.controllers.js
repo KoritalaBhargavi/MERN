2e31996200e3460ff84cfef8b83a5d10a16d47ab
koritala bhargavi <koritalabhargavi5@gmail.com>
2024-08-17 15:40:15 +0530

    Add files via upload

---
+++ b18/zoho/src/controllers/user.controllers.js
@@ -0,0 +1,118 @@
+const TUser = require('../model/user.model.js');
+
+exports.findAll = (req, res) => {
+TUser.find()
+  .then(users => {
+  res.send(users);
+}).catch(err => {
+  res.status(500).send({
+  message: err.message || "Something went wrong while getting list of users."
+});
+});
+};
+
+exports.create = (req, res) => {
+
+if(!req.body) {
+  return res.status(400).send({
+  message: "Please fill all required field"
+});
+}
+
+const user = new TUser({
+  tid:req.body.tid,
+  tage:req.body.tage,
+  tname:req.body.tname,
+  taddress:req.body.taddress,
+  tphone:req.body.tphone,
+  temail:req.body.temail,
+  tdob:req.body.tdob
+});
+
+user.save()
+  .then(data => {
+  res.send(data);
+}).catch(err => {
+  res.status(500).send({
+  message: err.message || "Something went wrong while creating new user."
+});
+});
+};
+
+exports.findOne = (req, res) => {
+ TUser.findById(req.params.id)
+  .then(user => {
+  if(!user) {
+   return res.status(404).send({
+   message: "User not found with id " + req.params.id
+ });
+}
+ res.send(user);
+}).catch(err => {
+  if(err.kind === 'ObjectId') {
+    return res.status(404).send({
+    message: "User not found with id " + req.params.id
+  });
+}
+return res.status(500).send({
+  message: "Error getting user with id " + req.params.id
+});
+});
+};
+
+exports.update = (req, res) => {
+
+if(!req.body) {
+  return res.status(400).send({
+  message: "Please fill all required field"
+});
+}
+
+TUser.findByIdAndUpdate(req.params.id, {
+  tid:req.body.tid,
+  tage:req.body.tage,
+  tname:req.body.tname,
+  taddress:req.body.taddress,
+  tphone:req.body.tphone,
+  temail:req.body.temail,
+  tdob:req.body.tdob
+}, {new: true})
+.then(user => {
+ if(!user) {
+   return res.status(404).send({
+   message: "user not found with id " + req.params.id
+ });
+}
+res.send(user);
+}).catch(err => {
+if(err.kind === 'ObjectId') {
+  return res.status(404).send({
+  message: "user not found with id " + req.params.id
+});
+}
+return res.status(500).send({
+  message: "Error updating user with id " + req.params.id
+});
+});
+};
+
+exports.delete = (req, res) => {
+TUser.findById(req.params.id)
+.then(user => {
+if(!user) {
+  return res.status(404).send({
+  message: "user not found with id " + req.params.id
+});
+}
+res.send({message: "user deleted successfully!"});
+}).catch(err => {
+if(err.kind === 'ObjectId' || err.name === 'NotFound') {
+  return res.status(404).send({
+  message: "user not found with id " + req.params.id
+});
+}
+return res.status(500).send({
+  message: "Could not delete user with id " + req.params.id
+});
+});
+};
---
+++ b18/zoho/src/route/user.route.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const router = express.Router()
+const userController = require('../controllers/user.controllers');
+router.get('/', userController.findAll);
+router.post('/', userController.create);
+router.get('/:id', userController.findOne);
+router.put('/:id', userController.update);
+router.delete('/:id', userController.delete);
+module.exports = router
+
