a43fc8c33d5e3343546660c445e5f7ba254dee7f
koritala bhargavi <koritalabhargavi5@gmail.com>
2024-08-17 15:41:51 +0530

    Add files via upload

---
+++ b19/firstapp/src/App.js
@@ -0,0 +1,71 @@
+import img from './logo.svg';
+import video from './video.mp4';
+import audio from './audio.wav';
+
+
+import './App.css';
+
+
+
+//export default App;
+
+import React, {Component} from 'react';
+class Fruits extends React.Component {
+  render(){
+    return (
+      <div class="container">
+        <Apple/>
+        <Banana/>
+        <Pine/>
+        <Grape/>
+        </div>
+    );
+  }
+}
+class Apple extends React.Component{
+  render(){
+    return(
+      <div>
+      <img src={img} width="200px" height="100%"/>
+      </div>
+    );
+  }
+}
+
+class Banana extends React.Component{
+  render(){
+    return(
+      <div>
+        <video src={video} controls width="200px"/>
+      </div>
+    );
+  }
+}
+
+class Pine extends React.Component{
+  render(){
+    return(
+      <div>
+        <audio src={audio} controls width="100%"/>
+      </div>
+    );
+  }
+}
+
+class Grape extends React.Component{
+  render(){
+    return(
+      <div>
+        <form>
+        <h1>Login</h1>
+        <label for="email" id="email">Email:</label>
+        <input type="email" name="email" size="30" required /><br/>
+        <label for="passid" id="passid">Password:</label>
+        <input type="password" name="passid" size="12" required /><br/>
+        <input type="submit" name="submit" value="Login" />
+        </form>
+      </div>
+    );
+  }
+}
+export default Fruits;
